a7492ae3c2e6a81543a160f63cbffbcee123756a
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2020-04-16 13:36:23 +0800

    Add rule to float bluetooth settings

--- awesome/.config/awesome/configuration/client/rules.lua
+++ awesome/.config/awesome/configuration/client/rules.lua
@@ -1,4 +1,5 @@
 local awful = require("awful")
+local beautiful = require("beautiful")
 local gears = require("gears")
 
 local client = require("configuration.keybindings.client")
@@ -12,6 +13,8 @@ awful.rules.rules =
         {
             above = false,
             below = false,
+            border_color = beautiful.border_normal,
+            border_width = beautiful.border_width,
             buttons = client.buttons,
             floating = false,
             focus = awful.client.focus.filter,
@@ -20,23 +23,46 @@ awful.rules.rules =
             maximized_horizontal = false,
             maximized_vertical = false,
             ontop = false,
-            placement = awful.placement.no_offscreen,
+            placement = awful.placement.no_overlap + awful.placement.no_offscreen,
             raise = true,
             screen = awful.screen.preferred,
             sticky = false
         }
     },
-    -- Titlebars
+    -- Floating clients
+    {
+        rule_any =
+        {
+            class =
+            {
+                "Blueman-adapters",
+                "Blueman-manager",
+            },
+            role =
+            {
+                "AlarmWindow",  -- Thunderbird's calendar.
+                "ConfigManager",  -- Thunderbird's about:config.
+                "pop-up",  -- e.g. Google Chrome's (detached) Developer Tools.
+            },
+            type =
+            {
+                "dialog"
+            },
+        },
+        properties =
+        {
+            floating = true
+        }
+    },
+    -- Dialogs
     {
         rule_any =
         {
             type = { "dialog" },
-            class = { "Wicd-client.py", "calendar.google.com" }
         },
         properties =
         {
             drawBackdrop = true,
-            floating = true,
             ontop = true,
             placement = awful.placement.centered,
             skip_decoration = true
