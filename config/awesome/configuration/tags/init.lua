3764b141184fd447ad06aaa2e1c6341648c3e285
Sandeep Singh Sidhu <sandeep01@hotmail.sg>
2020-05-20 09:52:44 +0800

    [awesome] Add one more tag and change default layouts

--- config/awesome/configuration/tags/init.lua
+++ config/awesome/configuration/tags/init.lua
@@ -2,48 +2,62 @@ local awful = require("awful")
 
 local icons = require("theme.icons.tags")
 
-local tags =
-{
-    {
-        title = "browser",
-        icon = icons.browser
-    },
-    {
-        title = "file",
-        icon = icons.folder
-    },
-    {
-        title = "tty",
-        icon = icons.temp
-    },
-    {
-        title = "{}",
-        icon = icons.code
-    },
-    {
-        title = "lab",
-        icon = icons.lab
-    },
-    {
-        title = "game",
-        icon = icons.game
-    },
-    {
-        title = "social",
-        icon = icons.social
-    },
-    {
-        title = "music",
-        icon = icons.music
-    }
-}
-
 awful.layout.layouts =
 {
     -- First layout is the default layout for all tags
     awful.layout.suit.tile,
+    awful.layout.suit.tile.bottom,
     awful.layout.suit.max,
     awful.layout.suit.floating
+}
+
+local tags =
+{
+    {
+        title = "browser",
+        icon = icons.browser,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "file",
+        icon = icons.folder,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "tty",
+        icon = icons.temp,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "{}",
+        icon = icons.code,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "lab",
+        icon = icons.lab,
+        awful.layout.suit.floating
+    },
+    {
+        title = "game",
+        icon = icons.game,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "social",
+        icon = icons.social,
+        layout = awful.layout.layouts[1]
+    },
+    {
+        title = "music",
+        icon = icons.music,
+        layout = awful.layout.suit.max,
+    },
+    {
+        title = "lab",
+        icon = icons.lab,
+        awful.layout.suit.floating
+    },
 }
 
 local count = 0;
@@ -56,11 +70,11 @@ awful.screen.connect_for_each_screen(
         for i, tag in pairs(tags) do
             count = count + 1;
 
-            awful.tag.add(tags[i].title,
+            awful.tag.add(tag.title,
             {
                 -- icon = tag.icon,
                 -- icon_only = true,
-                layout = awful.layout.layouts[1],
+                layout = tag.layout,
                 screen = s,
                 selected = i == 1
             })
