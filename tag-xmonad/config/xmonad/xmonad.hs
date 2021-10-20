74229a50a847f9660315521df75a2c8ac14f5797
Sandeep Singh <sandeep01@hotmail.sg>
2021-10-19 19:52:36 -0700

    Add custom workspaces and update theme for them

--- tag-xmonad/config/xmonad/xmonad.hs
+++ tag-xmonad/config/xmonad/xmonad.hs
@@ -6,16 +6,25 @@
 
 import XMonad
 
+import qualified XMonad.StackSet as StackSet
+import qualified Data.Map as Map
+
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.StatusBar
 
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed
 import XMonad.Layout.Spacing
+import XMonad.Layout.WindowNavigation
 
 import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad.Util.SpawnOnce
+import Data.Maybe
 -- import XMonad.Util.Ungrab
 
 -- !~~~~~||--------||------||~~~~~~
@@ -32,9 +41,11 @@ main = xmonad
 myConfig = def
     {
         modMask = mod4Mask,   -- Rebind Mod to the super key
-        layoutHook = myLayout, -- Use custom layouts
-        manageHook = myManageHook, -- Match on certain windows
+        layoutHook = myLayoutHook, -- Use custom layouts
+        manageHook = myManageHook <+> manageDocks, -- Match on certain windows
+        handleEventHook = docksEventHook,
         startupHook = myStartup,
+        workspaces = myWorkspaces,
         terminal = appTerminal
     }
     `additionalKeysP` myKeys
@@ -64,6 +75,19 @@ appTerminal = "kitty"
 configBrightnessStep :: Int
 configBrightnessStep = 10
 
+configBorderWidth :: Dimension
+configBorderWidth = 2 -- Sets border width for windows
+
+windowCount :: X (Maybe String)
+windowCount = gets $ Just
+    . show
+    . length
+    . StackSet.integrate'
+    . StackSet.stack
+    . StackSet.workspace
+    . StackSet.current
+    . windowset
+
 -- !~~~~~||--------||-----------||~~~~~~
 -- !~~~~~|| XMONAD || AUTOSTART ||~~~~~~
 -- !~~~~~||--------||-----------||~~~~~~
@@ -75,15 +99,52 @@ myStartup = do
     spawnOnce "xsetroot -cursor_name left_ptr"
     spawnOnce "/usr/lib/geoclue-2.0/demos/agent" -- Geolocation for redshift
     spawnOnce "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1" -- GUI authentication agent
+
     spawnOnce "autorandr --change && nitrogen --restore"
     spawnOnce "picom --experimental-backends -b"
 
     spawnOnce "blueman-applet &"
-    spawnOnce "nm-applet &"
-    spawnOnce "~/.local/bin/systray.sh &"
+    spawnOnce "nm-applet --sm-disable &"
     spawnOnce "volumeicon &"
+    spawnOnce "~/.local/bin/systray.sh"
 
     spawnOnce "pcmanfm --daemon-mode" -- Start PCManFM as a daemon to automatically mount removable media
+
+-- !~~~~~||--------||---------||~~~~~~
+-- !~~~~~|| XMONAD || LAYOUTS ||~~~~~~
+-- !~~~~~||--------||---------||~~~~~~
+
+-- Makes setting the spacingRaw simpler to write. The spacingRaw module adds a
+-- configurable amount of space around windows.
+mySpacing :: Integer -> l a -> ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+
+-- Below is a variation of the above except no borders are applied
+-- if fewer than two windows. So a single window has no gaps.
+mySpacing' :: Integer -> l a -> ModifiedLayout Spacing l a
+mySpacing' i = spacingRaw True (Border i i i i) True (Border i i i i) True
+
+tall = renamed [Replace "tall"]
+    $ windowNavigation
+    $ withBorder configBorderWidth
+    $ lessBorders Screen
+    $ smartSpacing 4
+    $ Tall 1 (3/100) (1/2)
+
+myLayout = tall ||| Mirror tiled ||| Full
+    where
+        tiled = Tall nmaster delta ratio
+        nmaster = 1 -- Default number of windows in the master pane
+        ratio = 1 / 2 -- Default proportion of screen occupied by master pane
+        delta = 3 / 100 -- Percent of screen to increment by when resizing panes
+
+myLayoutHook = tall ||| Full
+
+-- !~~~~~||--------||------------||~~~~~~
+-- !~~~~~|| XMONAD || WORKSPACES ||~~~~~~
+-- !~~~~~||--------||------------||~~~~~~
+
+myWorkspaces = [" dev ", " www ", " sys ", " doc ", " vbox ", " chat ", " mus ", " vid ", " gfx "]
 
 -- !~~~~~||--------||-------------||~~~~~~
 -- !~~~~~|| XMONAD || KEYBINDINGS ||~~~~~~
@@ -117,7 +178,7 @@ myKeys =
         -- Media Controls
         ("<XF86AudioPlay>", spawn "playerctl play-pause || mpc toggle"),
         ("M-<XF86AudioMute>", spawn "playerctl play-pause || mpc toggle"),
-        
+
         ("<XF86AudioNext>", spawn "playerctl next || mpc next"),
         ("M-<XF86AudioRaiseVolume>", spawn "playerctl next || mpc next"),
 
@@ -138,16 +199,6 @@ myKeys =
         -- ("M-S-=", unGrab *> spawn "scrot -s"),
     ]
 
--- !~~~~~||--------||--------||~~~~~~
--- !~~~~~|| XMONAD || LAYOUT ||~~~~~~
--- !~~~~~||--------||--------||~~~~~~
-myLayout = smartSpacing 3 $ tiled ||| Mirror tiled ||| Full
-    where
-        tiled    = Tall nmaster delta ratio
-        nmaster  = 1     -- Default number of windows in the master pane
-        ratio    = 1/2   -- Default proportion of screen occupied by master pane
-        delta    = 3/100 -- Percent of screen to increment by when resizing panes
-
 barSpawner :: ScreenId -> IO StatusBarConfig
 barSpawner id = pure $ statusBarProp ("xmobar -x " ++ show (toInteger id)) $ pure myXmobarPP -- nothing on the rest of the screens
 
@@ -161,14 +212,16 @@ myManageHook = composeAll
 myXmobarPP :: PP
 myXmobarPP = def
     {
-        ppSep             = magenta " • ",
+        ppSep = "<fc=#666666> <fn=1>|</fn> </fc>", -- Separator character
+        ppTitle = xmobarColor "#b3afc2" "" . shorten 60, -- Title of active window
         ppTitleSanitize   = xmobarStrip,
-        ppCurrent         = wrap " " "" . xmobarBorder "Top" "#8be9fd" 2,
-        ppHidden          = white . wrap " " "",
-        ppHiddenNoWindows = lowWhite . wrap " " "",
-        ppUrgent          = red . wrap (yellow "!") (yellow "!"),
-        ppOrder           = \[ws, l, _, wins] -> [ws, l, wins],
-        ppExtras          = [logTitles formatFocused formatUnfocused]
+        ppCurrent = xmobarColor "#c792ea" "" . wrap "<box type=Bottom width=2 mb=2 color=#c792ea>" "</box>", -- Current workspace
+        ppVisible = xmobarColor "#c792ea" "",              -- Visible but not current workspace
+        ppHidden = xmobarColor "#82AAFF" "" . wrap "<box type=Top width=2 mt=2 color=#82AAFF>" "</box>", -- Hidden workspaces
+        ppHiddenNoWindows = xmobarColor "#82AAFF" "", -- Hidden workspaces (no windows)
+        ppUrgent = xmobarColor "#C45500" "" . wrap "!" "!", -- Urgent workspace
+        ppOrder = \(ws : l : t : ex) -> [ws, l] ++ ex ++ [t], -- order of things in xmobar
+        ppExtras = [windowCount]
     }
   where
     formatFocused   = wrap (white    "[") (white    "]") . magenta . ppWindow
